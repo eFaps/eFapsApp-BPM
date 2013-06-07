b3c42b69af50e2487eecdc95acf211572fe2285c
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-06-07 02:35:33 +0000

    - bpm: the processImage is read from the calling command
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/bpm/trunk@9581 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/bpm/BProcess_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/bpm/BProcess_Base.java
@@ -36,10 +36,12 @@ import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.admin.ui.AbstractCommand;
 import org.efaps.bpm.BPM;
 import org.efaps.ci.CIAdminProgram;
 import org.efaps.db.Checkout;
 import org.efaps.db.Instance;
+import org.efaps.db.InstanceQuery;
 import org.efaps.db.MultiPrintQuery;
 import org.efaps.db.QueryBuilder;
 import org.efaps.db.SelectBuilder;
@@ -48,6 +50,8 @@ import org.efaps.esjp.bpm.image.jobs.TaskColorJob;
 import org.efaps.esjp.ci.CIBPM;
 import org.efaps.util.EFapsException;
 import org.jbpm.task.query.TaskSummary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * TODO comment!
@@ -59,7 +63,19 @@ import org.jbpm.task.query.TaskSummary;
 @EFapsRevision("$Rev$")
 public abstract class BProcess_Base
 {
+    /**
+     * Logging instance used in this class.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(BProcess_Base.class);
 
+    /**
+     * @param _parameter Parameter as passed by the eFaps API
+     * @param _taskName name of the task
+     * @param _status list of status to be used as filter, <code>null</code>
+     *            uses all
+     * @return list of taskSummary
+     * @throws EFapsException on error
+     */
     public List<TaskSummary> getTaskSummary4Instance(final Parameter _parameter,
                                                      final String _taskName,
                                                      final List<org.jbpm.task.Status> _status)
@@ -94,7 +110,12 @@ public abstract class BProcess_Base
         return ret;
     }
 
-    public Return getImageFieldValue(final Parameter _parameter)
+    /**
+     * @param _parameter Parameter as passed by the eFaps API
+     * @return Return with svg snipplet
+     * @throws EFapsException on error
+     */
+    public Return getProcessImageFieldValue(final Parameter _parameter)
         throws EFapsException
     {
         final Return ret = new Return();
@@ -129,31 +150,32 @@ public abstract class BProcess_Base
         return ret;
     }
 
+    /**
+     * @param _parameter Parameter as passed by the eFaps API
+     * @return Return with svg snipplet
+     * @throws EFapsException on error
+     */
     public Return getProcessImage4InstanceFieldValue(final Parameter _parameter)
         throws EFapsException
     {
         final Return ret = new Return();
         final StringBuilder html = new StringBuilder();
+        final Instance imageInst = getProcessImageInstance(_parameter);
+        if (imageInst.isValid()) {
+            final QueryBuilder queryBldr = new QueryBuilder(CIBPM.GeneralInstance2ProcessId);
+            queryBldr.addWhereAttrEqValue(CIBPM.GeneralInstance2ProcessId.GeneralInstanceLink, _parameter.getInstance()
+                            .getGeneralId());
+            final MultiPrintQuery multi = queryBldr.getPrint();
+            multi.addAttribute(CIBPM.GeneralInstance2ProcessId.ProcessId);
+            multi.executeWithoutAccessCheck();
 
-        final QueryBuilder queryBldr = new QueryBuilder(CIBPM.GeneralInstance2ProcessId);
-        queryBldr.addWhereAttrEqValue(CIBPM.GeneralInstance2ProcessId.GeneralInstanceLink, _parameter.getInstance()
-                        .getGeneralId());
-        final MultiPrintQuery multi = queryBldr.getPrint();
-        multi.addAttribute(CIBPM.GeneralInstance2ProcessId.ProcessId);
-        multi.executeWithoutAccessCheck();
+            final List<NodeInstance> nodes = new ArrayList<NodeInstance>();
+            while (multi.next()) {
+                final Long processInstanceId = multi.<Long>getAttribute(CIBPM.GeneralInstance2ProcessId.ProcessId);
+                nodes.addAll(BPM.getActiveNodes4ProcessId(processInstanceId));
+            }
 
-        final List<NodeInstance> nodes = new ArrayList<NodeInstance>();
-        while (multi.next()) {
-            final Long processInstanceId = multi.<Long>getAttribute(CIBPM.GeneralInstance2ProcessId.ProcessId);
-            nodes.addAll(BPM.getActiveNodes4ProcessId(processInstanceId));
-        }
-
-        final QueryBuilder imgQueryBldr = new QueryBuilder(CIAdminProgram.BPMImage);
-        imgQueryBldr.addWhereAttrEqValue(CIAdminProgram.BPMImage.Name, "Pecsa_IncomingOrderProcess");
-        final MultiPrintQuery query = imgQueryBldr.getPrint();
-        query.execute();
-        while (query.next()) {
-            final Checkout checkOut = new Checkout(query.getCurrentInstance());
+            final Checkout checkOut = new Checkout(imageInst);
             final InputStream svgIn = checkOut.execute();
             try {
                 final Processor processor = new Processor(svgIn);
@@ -163,15 +185,38 @@ public abstract class BProcess_Base
                 processor.applyTransformationJobs();
                 html.append(processor.toXML());
             } catch (final IOException e) {
-                e.printStackTrace();
+                BProcess_Base.LOG.error("IOException", e);
             }
         }
-
         if (html.length() < 1) {
             html.append(DBProperties.getProperty("org.efaps.esjp.bpm.BProcess.NoImage"));
         }
         ret.put(ReturnValues.SNIPLETT, html.toString());
         return ret;
+    }
 
+    /**
+     *  @param _parameter Parameter as passed by the eFaps API
+     * @return istance for an BPMImage
+     * @throws EFapsException on error
+     */
+    protected Instance getProcessImageInstance(final Parameter _parameter)
+        throws EFapsException
+    {
+        Instance ret = Instance.get("");
+        final AbstractCommand cmd = (AbstractCommand) _parameter.get(ParameterValues.CALL_CMD);
+        if (cmd == null) {
+            BProcess_Base.LOG.error("no command found");
+        } else {
+            final String uuid = cmd.getProperty("ProcessImageUUID");
+            final QueryBuilder imgQueryBldr = new QueryBuilder(CIAdminProgram.BPMImage);
+            imgQueryBldr.addWhereAttrEqValue(CIAdminProgram.BPMImage.UUID, uuid);
+            final InstanceQuery query = imgQueryBldr.getQuery();
+            query.execute();
+            if (query.next()) {
+                ret = query.getCurrentValue();
+            }
+        }
+        return ret;
     }
 }
